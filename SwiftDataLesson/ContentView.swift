172664acf3d5d60598eb8bd63d3e2846263f34fc
Vlad Yahnovec <vladyahnovec@gmail.com>
2024-04-21 13:10:13 +0300

    Environment object for the context

--- SwiftDataLesson/ContentView.swift
+++ SwiftDataLesson/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var context
     var body: some View {
         HStack {
             Text("Clear")
